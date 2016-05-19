ac1457c991ed86b924250626c683bc6dfb2fe376
Denis Surin <pqdubpq@gmail.com>
2016-05-19 23:47:12 +0200

    Added check_polidrom Function

--- dz2-7.php
+++ dz2-7.php
@@ -1,3 +1,4 @@
+<?php
 /*
     Функция принимает 1 строковый параметр и возвращает TRUE, если строка
     является палиндромом, FALSE в противном случае. Пробелы не должны учитываться.
@@ -8,3 +9,31 @@
     Дополнительно (не обязательно): Искала бы все палиндромы, в строке начиная с
     трёх символов.
 */
+
+    function is_palindrom ($string) {
+
+        echo "Строка \"$string\"";
+
+        $result = check_palindrom($string);
+
+        if ($result === true)
+            echo ' является палиндромом!';
+
+        else if ($result === -1)
+            echo ' является недопустимым параметром';
+
+        else
+            echo ' не является палиндромом!';
+    }
+
+    function check_palindrom ($str) {
+
+        if ( !(is_string($str)) )
+            return -1;
+
+        $str = mb_strtolower(str_replace(' ', '', $str));
+
+        $reverse_str = string_reverse($str);
+
+        return $str === $reverse_str;
+    }
